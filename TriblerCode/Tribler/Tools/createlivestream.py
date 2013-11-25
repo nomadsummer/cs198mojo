b343e892355c9ccaece10cf9a7695cb17887c2a3
Elijah Joshua Cayabyab <ebcayabyab01@gmail.com>
2013-11-25 15:01:18 +0800

    ....

--- TriblerCode/Tribler/Tools/createlivestream.py
+++ TriblerCode/Tribler/Tools/createlivestream.py
@@ -335,7 +335,7 @@ def mjcompute_criterion(ds, mjpeers):
                 print >>sys.stderr,"Calling the getHelp() function..."
                 #x.update("HELPED", True)
                 mjbandwidth_allocation(ds)
-                getHelp(x.data["highpeers"], x.data["lowpeers"])
+                #getHelp(x.data["highpeers"], x.data["lowpeers"])
 
 def mjbandwidth_allocation(ds):
     if(x.is_existing("MIN-NEEDED")):
@@ -432,6 +432,7 @@ def mjcallback(addr, msg):
         
         # Reply to the helped swarm with your peer list
         x.log("highpeers", s.get_external_ip())
+        print >>sys.stderr, "high peer list" , x.data["highpeers"]
         MojoCommunicationClient(MJ_LISTENPORT,'[ACK-HELP]XxX+XxX' + pickle.dumps(x.data["highpeers"]) + 'XxX+XxX' + pickle.dumps(x.data["lowpeers"]), addr[0])
         """
     elif msg.startswith('[criterionrep]'):
