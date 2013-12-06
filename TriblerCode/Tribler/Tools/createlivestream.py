b6f8c80a427ac77db62156089bb7f2c1956f89c1
Elijah Joshua Cayabyab <ebcayabyab01@gmail.com>
2013-12-06 16:43:34 +0800

    Display bandwidth alloc

--- TriblerCode/Tribler/Tools/createlivestream.py
+++ TriblerCode/Tribler/Tools/createlivestream.py
@@ -332,7 +332,7 @@ def mjcompute_rankings():
         counter = counter + 1
         print >>sys.stderr,"help counter", counter
         #if(x.data["CIRI"][0] < 1):
-        if counter == 30 and not x.data["HELPING"][0]:
+        if counter == 5 and not x.data["HELPING"][0]:
             if(x.is_existing("highpeers")):
                 x.delete("highpeers")   
             if(x.is_existing("lowpeers")):
